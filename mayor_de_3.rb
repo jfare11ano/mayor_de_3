35da014aa24148c7b5f7253ade528ade0b0856dd
jfare11ano <jarellan@ccu.cl>
2019-05-09 20:34:27 -0400

    2do commit

--- mayor_de_3.rb
+++ mayor_de_3.rb
@@ -1,5 +1,53 @@
-arg1 = ARGV[0].to_i
-arg2 = ARGV[1].to_i
-arg3 = ARGV[2].to_i
+a = ARGV[0].to_f
+b = ARGV[1].to_f
+c = ARGV[2].to_f
 
-print [arg1,arg2,arg3].max
+if a>0
+    if b>0
+        if c>0
+            if a>b && a>c
+                puts a
+            elsif b>a && b>c
+                puts b
+            else 
+                puts c
+            end
+        else
+            if a>b
+                puts a
+            else 
+                puts b
+            end
+        end
+    else 
+        if c>0
+            if a>c
+                puts a
+            else
+                puts c
+            end
+        else
+            puts c
+        end
+    end
+else
+    if b>0
+        if c>b
+            puts c
+        else 
+            puts b
+        end
+    else
+        if c>0
+            puts c
+        else
+            if a>b && a>c
+                puts a
+            elsif b>a && b>c
+                puts b
+            else
+                puts c
+            end
+        end
+    end        
+end
